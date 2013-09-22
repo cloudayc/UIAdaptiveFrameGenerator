848093ecfcb9f56aa5ed79b4269623fa6e92c192
cloudayc <cloudayc@gmail.com>
2013-09-22 19:09:11 +0800

    update

---
+++ UIAdaptiveFrameGenerator/UIAdaptiveFrameGenerator/Model/SubviewObject.h
@@ -0,0 +1,13 @@
+//
+//  SubviewObject.h
+//  UIAdaptiveFrameGenerator
+//
+//  Created by wyq on 13-9-22.
+//  Copyright (c) 2013年 cloudayc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubviewObject : NSObject
+
+@end
---
+++ UIAdaptiveFrameGenerator/UIAdaptiveFrameGenerator/Model/SubviewObject.m
@@ -0,0 +1,13 @@
+//
+//  SubviewObject.m
+//  UIAdaptiveFrameGenerator
+//
+//  Created by wyq on 13-9-22.
+//  Copyright (c) 2013年 cloudayc. All rights reserved.
+//
+
+#import "SubviewObject.h"
+
+@implementation SubviewObject
+
+@end
